bcea10f4c1a812c246d8c42fe3452bd267df2a07
zakariaghrib <zikoghb12@gmail.com>
2026-05-13 23:59:24 +0200

    configuration

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8080/api',
+  baseURL: 'https://blog-backend-production-7404.up.railway.app/api',
   headers: {
     'Content-Type': 'application/json',
   },
